ac84b6e9c26a9a6e6211fa7b298f5d43c3f685ff
luisJavierLuna <javierlunaluis@gmail.com>
2022-05-03 10:57:44 -0500

    Added basic movement function to the player

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float speed = 10;
+    [SerializeField] Vector3 moveDirection;
+    const string HORIZONTAL = "Horizontal";
+    const string VERTICAL = "Vertical";
+    float h;
+    float v;
+
+    private void Update()
+    {
+        h = Input.GetAxisRaw(HORIZONTAL);
+        v = Input.GetAxisRaw (VERTICAL);
+        moveDirection.x = h;
+        moveDirection.y = v;
+
+        transform.position += moveDirection.normalized * speed * Time.deltaTime;
+    }
+
 
 }
