b5b9889edbf314b11e5d3aa220c2597b6ccab11d
luisJavierLuna <javierlunaluis@gmail.com>
2022-05-10 21:24:41 -0500

    Added Count Down Routine

--- GameManager.cs
+++ GameManager.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int time = 50;
 
+    private void Start()
+    {
+        StartCoroutine(CountDownRoutine());
+    }
+
+    IEnumerator CountDownRoutine()
+    {
+        while (time > 0)
+        {
+            yield return new WaitForSeconds(1);
+            time--;
+            if (time == 0) Debug.Log("Game Over");
+        }
+    }
 }
